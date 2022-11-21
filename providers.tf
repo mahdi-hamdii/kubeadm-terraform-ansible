468f8ee7d122c1b7c3ce21c265bfc44b8ccfdd33
Mahdi Hamdi <mahdi@pop-os.localdomain>
2022-11-21 12:44:18 +0100

    changing backend

--- providers.tf
+++ providers.tf
@@ -11,3 +11,13 @@ provider "azurerm" {
 
   subscription_id = var.subscription_id
 }
+
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "DevOps-GL5"
+    storage_account_name = "devopsterraz"
+    container_name       = "devops-gl5"
+    key                  = "terraform.tfstate"
+  }
+}
